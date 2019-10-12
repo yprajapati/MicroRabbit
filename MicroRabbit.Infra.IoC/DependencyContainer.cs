122f2d9ddfc3fea023d67449c34a1b010d4aacd8
daksh-yogendra <yogendra@kandooit.co>
2019-10-12 12:12:50 +0530

    Commit Changes

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -21,7 +21,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace MicroRabbit.Infra.IoC
-{
+{ 
     public class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services)
